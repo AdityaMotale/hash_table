b49b0ac9a47e6f5909f69c63ff36972fd3f141b5
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-09-09 00:35:10 +0530

    impl of hash_table w/ unit tests and example usage and benchmarks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::time::Instant;
+
+use table::HashTable;
+
+mod table;
+
 fn main() {
-    println!("Hello, world!");
+    let mut hash_table = HashTable::<String, usize>::new();
+
+    // benchmark insert operation
+    let start = Instant::now();
+
+    for i in 0..100000 {
+        let key = i.to_string();
+        hash_table.insert(key, i);
+    }
+    let duration = start.elapsed();
+
+    println!("Time taken for inserting 100K items: {:?}", duration);
+
+    // benchmark get operation
+    let start = Instant::now();
+
+    for i in 0..100000 {
+        let key = i.to_string();
+        hash_table.get(&key);
+    }
+
+    let duration = start.elapsed();
+
+    println!("Time taken for retrieval of 100K items: {:?}", duration);
+
+    // usage
+
+    let key = String::from("hello");
+
+    // insert an item
+    hash_table.insert(key.clone(), 2);
+
+    // get an item
+    if let Some(val) = hash_table.get(&key) {
+        println!("Value for key {key} is {}", val);
+    } else {
+        println!("Value for key {key} does not exists")
+    }
+
+    // update value for a key
+    if let Some(val) = hash_table.get_mut(&key) {
+        *val = 5;
+        println!("Updated value for {key} to {}", 5);
+    }
+
+    // get value for updated item
+    if let Some(val) = hash_table.get(&key) {
+        println!("Value for key {key} is {}", val);
+    } else {
+        println!("Value for key {key} does not exists")
+    }
 }
